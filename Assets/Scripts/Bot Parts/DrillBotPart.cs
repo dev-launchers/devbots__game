9e5d52c0248c89beb78f26bdf5c789e6ef7d26bc
LawrenceLinnell <76969895+LawrenceLinnell@users.noreply.github.com>
2021-08-14 21:46:47 -0400

    Update DrillBotPart.cs
    Implemented cool down for drill bot part   utilizing GetCoolDown() from BotPart parent.
    Changed public fields to private to improve security.

--- Assets/Scripts/Bot Parts/DrillBotPart.cs
+++ Assets/Scripts/Bot Parts/DrillBotPart.cs
@@ -6,12 +6,14 @@ public class DrillBotPart : BotPart
 {
     //TODO: create "private Animator drillAnimation;" // Animates the drill part when attack is active.
 
-    [SerializeField] public Transform attackPoint; // References the attack point of the drill in the scene.
-    [SerializeField] public float attackRange = 0.0f; // Range for attack to initiate.
+    [SerializeField] private Transform attackPoint; // References the attack point of the drill in the scene.
+    [SerializeField] private float attackRange = 0.0f; // Range for attack to initiate.
 
-    [SerializeField] public LayerMask enemyLayers;
+    [SerializeField] private LayerMask enemyLayers;
     [SerializeField] private bool isRunning;
     // Used to determine which objects are enemies by assigning all ememies to a layer using a layermask.
+
+    private float timer;
 
     // Inherited from BotPart
     public override void SetState(State state)
@@ -22,6 +24,7 @@ public class DrillBotPart : BotPart
     // Start is called before the first frame update
     void Start()
     {
+        timer = 0.0f;
         return;
     }
 
@@ -32,21 +35,27 @@ public class DrillBotPart : BotPart
     }
 
     // drillAttack
-    public void drillAttack()
+    private void drillAttack()
     {
         if (isRunning) {
-            // TODO:  Play the drill attack animation.
+            if (timer > 0) {
+                timer -= Time.deltaTime;
+            }
+            else {
+                timer = GetCoolDown();
+                // TODO:  Play the drill attack animation.
 
-            // Detect enemy in range of attack.
-            Collider2D enemy = Physics2D.OverlapCircle(attackPoint.position, attackRange, enemyLayers);
+                // Detect enemy in range of attack.
+                Collider2D enemy = Physics2D.OverlapCircle(attackPoint.position, attackRange, enemyLayers);
 
-            // Damage enemy
-            // TODO: Implement damage to enemy health.
+                // Damage enemy
+                // TODO: Implement damage to enemy health.
 
-            if(enemy)
-            {
-                // Outputs message to Unity Editor Console to verify the attack.
-                Debug.Log(enemy.name + " was attacked by drill.");
+                if(enemy)
+                {
+                    // Outputs message to Unity Editor Console to verify the attack.
+                    Debug.Log(enemy.name + " was attacked by drill.");
+                }
             }
         }
     }
